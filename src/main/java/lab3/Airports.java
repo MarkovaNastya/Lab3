2b695c51023e2b11bfdd9518ed5786c7328d6d6b
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-07 21:27:44 +0300

    Scripted auto-commit on change (2019-11-07 21:27:44) by gitwatch.sh

--- src/main/java/lab3/Airports.java
+++ src/main/java/lab3/Airports.java
@@ -10,20 +10,32 @@ public class Airports {
     private JavaPairRDD<Integer, String> airportsInfo;
     private JavaRDD<String> airportsTable;
 
+    private final static String QUOTES = "\"";
+    private final static String COMMA = ",";
+    private final static String EMPTY = "";
+
+    private final static int COUNT_COLUMNS = 2;
+
+    private final static int AIRPORTS_ID_COLIMN = 0;
+    private final static int AIRPORTS_DESCRIPTION_COLIMN = 1;
+
+
     public Airports(JavaSparkContext sc, String path) {
         JavaRDD<String> airportsTable = App.deleteTitle(sc.textFile(path));
     }
 
-    private String parseLine(String line) {
-        
+    private String parseLineGetPos(String line, int pos) {
+        String column = line.split(COMMA, COUNT_COLUMNS)[pos];
+        return column.replaceAll(QUOTES, EMPTY);
     }
 
     private JavaPairRDD<Integer, String> parseTable() {
-        airportsTable.mapToPair(
+        return airportsTable.mapToPair(
                 s -> {
-                    Integer id = s.split(",", 2)[0];
+                    Integer id = Integer.parseInt(parseLineGetPos(s, AIRPORTS_ID_COLIMN));
+                    String description = parseLineGetPos(s, AIRPORTS_DESCRIPTION_COLIMN);
 
-                    return Tuple2<>;
+                    return Tuple2<id, description>;
                 }
         );
 

bc4306d5b59598ed4099ae2ce8a1d42d59927301
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-07 21:07:26 +0300

    Scripted auto-commit on change (2019-11-07 21:07:26) by gitwatch.sh

--- src/main/java/lab3/Airports.java
+++ src/main/java/lab3/Airports.java
@@ -1,7 +1,13 @@
 package lab3;
 
+import org.apache.spark.api.java.JavaPairRDD;
+
 public class Airports {
 
-    
+    private JavaPairRDD<Integer, String> airportsInfo;
+
+    public Airports(String path) {
+        
+    }
 
 }

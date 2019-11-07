3098afc253d7fca95f82310b8663a8d5eb66fbfa
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-07 21:47:32 +0300

    Scripted auto-commit on change (2019-11-07 21:47:32) by gitwatch.sh

--- src/main/java/lab3/DelaysInfo.java
+++ src/main/java/lab3/DelaysInfo.java
@@ -1,5 +1,6 @@
 package lab3;
 
+import javafx.util.Pair;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -7,8 +8,8 @@ import scala.Tuple2;
 
 public class DelaysInfo {
 
-    private JavaPairRDD<Integer, Integer> delaysInfoIDs;
-    private float delaysInfoData[];
+    private JavaPairRDD<Pair<Integer, Integer>, float[]> delaysInfoIDs;
+//    private float delaysInfoData[];
     private JavaRDD<String> delaysTable;
 
     private final static String COMMA = ",";
@@ -20,7 +21,7 @@ public class DelaysInfo {
 
     public DelaysInfo(JavaSparkContext sc, String path) {
         delaysTable = App.deleteTitle(sc.textFile(path));
-        delaysInfoData = new float[COUNT_DATA_COLUMNS];
+//        delaysInfoData = new float[COUNT_DATA_COLUMNS];
     }
 
     private String parseLineGetPos(String line, int pos) {
@@ -32,8 +33,9 @@ public class DelaysInfo {
                 s -> {
                     Integer idFrom = Integer.parseInt(parseLineGetPos(s, ID_FROM_COLUMN));
                     Integer idTo = Integer.parseInt(parseLineGetPos(s, ID_TO_COLUMN));
+                    Pair<Integer, Integer> ids = new Pair<>(idFrom, idTo);
 
-                    return new Tuple2<>(idFrom, idTo);
+                    return ;
                 }
         );
 

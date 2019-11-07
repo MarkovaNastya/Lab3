40ce6f3f1e4f61cbac1d4f5b61db45c54f61bd50
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-07 21:33:41 +0300

    Scripted auto-commit on change (2019-11-07 21:33:41) by gitwatch.sh

--- src/main/java/lab3/DelaysInfo.java
+++ src/main/java/lab3/DelaysInfo.java
@@ -5,8 +5,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public class DelaysInfo {
 
-    public AirportsInfo(JavaSparkContext sc, String path) {
-        JavaRDD<String> airportsTable = App.deleteTitle(sc.textFile(path));
+
+
+    public DelaysInfo(JavaSparkContext sc, String path) {
+        JavaRDD<String> delaysTable = App.deleteTitle(sc.textFile(path));
     }
 
 }

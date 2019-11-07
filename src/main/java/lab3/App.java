f882067913ee9895d59ab931bc42321e10cda06d
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-07 22:21:07 +0300

    Scripted auto-commit on change (2019-11-07 22:21:07) by gitwatch.sh

--- src/main/java/lab3/App.java
+++ src/main/java/lab3/App.java
@@ -5,6 +5,9 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
+
+import java.util.Map;
 
 public class App {
 
@@ -24,8 +27,9 @@ public class App {
         AirportsInfo airportsTable = new AirportsInfo(sc, args[0]);
         airportsTable.parseTable();
         JavaPairRDD<Integer, String> airportsInfo = airportsTable.getAirportsInfo();
+        Map<Integer, String> airportsInfoMap = airportsInfo.collectAsMap();
 
-        final Broadcast<Map<String, AirportData>> airportsBroadcasted = sc.broadcast(stringAirportDataMap);
+        final Broadcast<Map<Integer, String>> airportsBroadcasted = sc.broadcast(airportsInfoMap);
 
         DelaysInfo delaysTable = new DelaysInfo(sc, args[1]);
         delaysTable.parseTable();
